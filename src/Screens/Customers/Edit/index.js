cc259bc10457aadbb60a501f31e050f2f985f1e7
prosenjeetshil <prosenjeetshil@gmail.com>
2023-03-03 21:54:57 +0530

    feat: edit existing customers

---
+++ src/Screens/Customers/Edit/index.js
@@ -0,0 +1,10 @@
+import { Edit } from "../../../features/customers/Edit";
+import { SafeAreaView, ScrollView } from "react-native";
+
+export const EditScreen = () => (
+  <SafeAreaView>
+    <ScrollView>
+      <Edit />
+    </ScrollView>
+  </SafeAreaView>
+);
