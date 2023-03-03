9381fc9c2a9072364c38809ee47b7e45816fa7a9
prosenjeetshil <prosenjeetshil@gmail.com>
2023-03-03 21:55:48 +0530

    feat: style for regions screen

---
+++ src/Screens/Regions/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const style = () =>
+  StyleSheet.create({
+    container: {
+      backgroundColor: "#ECF2FF",
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    text: {
+      fontSize: 20,
+      fontWeight: "bold",
+      marginBottom: 20,
+    },
+  });
