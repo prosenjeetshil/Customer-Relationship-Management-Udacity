328394a4fbfe4cd480cc30be4c681192bbbb1c54
prosenjeetshil <prosenjeetshil@gmail.com>
2023-03-03 21:54:35 +0530

    feat: create customer

---
+++ src/Screens/Customers/Create/index.js
@@ -0,0 +1,10 @@
+import { Create } from "../../../features/customers/Create";
+import { SafeAreaView, ScrollView } from "react-native";
+
+export const CreateScreen = () => (
+  <SafeAreaView>
+    <ScrollView>
+      <Create />
+    </ScrollView>
+  </SafeAreaView>
+);
